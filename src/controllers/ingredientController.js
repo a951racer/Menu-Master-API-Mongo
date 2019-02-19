f161089fab9ac12d5623bb40f2b2e3f8e13783c3
a951racer <jon.hobbs@minnykid.com>
2019-02-18 20:55:54 -0600

    Updates to implement a CSV import for ingredients

--- src/controllers/ingredientController.js
+++ src/controllers/ingredientController.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const IncomingForm = require('formidable').IncomingForm;
+const fs = require('fs');
+const parse = require('csv-parse/lib/sync');
 
 const Ingredient = mongoose.model('Ingredient');
 
 exports.addNewIngredient = (req, res) => {
     let newIngredient = new Ingredient(req.body);
-
     newIngredient.save((err, ingredient) => {
         if (err) {
             res.send(err);
@@ -50,4 +52,28 @@ exports.deleteIngredient = (req, res) => {
         }
         res.json({ message: 'Successfully deleted Ingredient'});
     })
+}
+
+exports.upload = (req, res) => {
+    var form = new IncomingForm();
+    let readStream;
+    form.on('file', (field, file) => {
+      fs.readFile(file.path, (err, data) => {
+        if (err) console.error(err);
+        const records = parse(data, {
+            columns: true,
+            skip_empty_lines: true
+          });
+        records.forEach(ingredient => {
+            let newIngredient = new Ingredient(ingredient);
+            newIngredient.save((err, ingredient) => {
+                if (err) console.error(err);
+            });
+        });
+      })
+    });
+    form.on('end', () => {
+      res.json();
+    });
+    form.parse(req);
 }
